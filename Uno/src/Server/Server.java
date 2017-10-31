f43f6f5c9820746979adbd55c56fb8fe3d5c0de8
Alunno <Alunno@PC-13-INF4>
2017-10-31 13:02:12 +0100

    Server e serverMatch

---
+++ Uno/src/Server/Server.java
@@ -0,0 +1,39 @@
+package Server;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Server extends Thread{
+	
+	private ServerSocket serverSocket;
+	private boolean isRunning = true;
+	private ArrayList<ServerMatch> matches;
+	public Server(int port) throws IOException {
+		this.serverSocket = new ServerSocket(((port > 1000) ? port : 9999));
+		this.matches = new ArrayList<ServerMatch>();
+	}
+	
+	public void run() {
+		int cont = -1;
+		while(isRunning) {
+			try {
+				Socket home = this.serverSocket.accept(); //Si connette il primo giocatore di una partita
+				Socket away = this.serverSocket.accept(); //Si connette il secondo
+				ServerMatch sm = (new ServerMatch(home,away)); //Creo la partita
+				this.matches.add(sm); //Aggiungo la partita alla lista di partite attive
+				sm.start(); //Inizia la partita
+				
+			} catch (IOException e) {
+				e.printStackTrace();
+				this.isRunning = false;
+			}
+		}
+	}
+	
+	public void kill() {
+		this.isRunning = false;
+	}
+
+}
---
+++ Uno/src/Server/ServerMatch.java
@@ -0,0 +1,42 @@
+package Server;
+
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ServerMatch extends Thread{
+	
+	private Socket home = null; //Giocatore 1
+	private Socket away = null; //Giocatore 2
+	private ObjectOutputStream homeOutStream;
+	private ObjectOutputStream awayOutStream;
+	private Mazzo mazzo = null;
+	private Mano manoHome = null;
+	private Mano manoAway = null;
+	
+	private boolean isRunning = true;
+	
+	public ServerMatch(Socket home, Socket away) {
+		this.home = home;
+		this.away = away;
+
+		this.homeOutStream = new ObjectOutputStream(this.home.getOutputStream());
+		this.awayOutStream = new ObjectOutputStream(this.away.getOutputStream());
+	}
+	
+	public void run() {
+		//Inizia la partita
+		for(int i = 0; i < 7; i++) { //Consegno 7 carte
+			Carta cHome = mazzo.pesca();
+			Carta cAway = mazzo.pesca();
+			
+		}
+		while(this.isRunning) {
+			
+		}
+	}
+	
+	public void kill() {
+		this.isRunning = false;
+	}
+
+}
