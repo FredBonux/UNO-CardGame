60d6d8573aae86cb4eaa32b971a9906d3521c891
Alunno <Alunno@PC-12-INF4>
2017-11-07 12:55:38 +0100

    Dorin

--- Uno/src/Client/PacketCont.java
+++ Uno/src/Client/PacketCont.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import Carte.Carta;
 import Carte.Mano;
 import Carte.Mazzo;
+import Carte.Tavolo;
 import Utility.CTRLPacket;
 import Utility.Packet;
 
@@ -58,18 +59,18 @@ public class PacketCont extends CTRLPacket{
 	public void subisciNormale(Carta c) {
 		// TODO Auto-generated method stub
 		
-		int ris,flag=0; 
+		int flag=0; 
 		for(int i=0;i<mano.getMano().size();i++){
 			
-			if(pushToTavoloControl(mano.getMano().get(i))){
+			if(tavolo.pushToTavoloControl(mano.getMano().get(i))){
 				Carta app=mano.getMano().get(i);
-				tavolo.pushToTavolo(app)
+				tavolo.pushToTavolo(app);
 				flag = 1;
 				break;
 			}
 		}
 		if(flag!=1){
-			mano.aggiungiCarta(Mazzo.pesca())
+			mano.aggiungiCarta(mazzo.pesca());
 		}
 		
 	}
@@ -77,18 +78,51 @@ public class PacketCont extends CTRLPacket{
 	@Override
 	public void subisciPIU2(Carta piu2, Carta c1, Carta c2) {
 		// TODO Auto-generated method stub
+		mano.aggiungiCarta(c1);
+		mano.aggiungiCarta(c2);
 		
+		int flag=0; 
+		for(int i=0;i<mano.getMano().size();i++){
+			
+			if(tavolo.pushToTavoloControl(mano.getMano().get(i))){
+				Carta app=mano.getMano().get(i);
+				tavolo.pushToTavolo(app);
+				flag = 1;
+				break;
+			}
+		}
+		if(flag!=1){
+			mano.aggiungiCarta(mazzo.pesca());
+		}
 	}
 
 	@Override
 	public void subisciPIU4(Carta piu4, Carta c1, Carta c2, Carta c3, Carta c4) {
 		// TODO Auto-generated method stub
+		mano.aggiungiCarta(c1);
+		mano.aggiungiCarta(c2);
+		mano.aggiungiCarta(c3);
+		mano.aggiungiCarta(c4);
 		
+		int flag=0; 
+		for(int i=0;i<mano.getMano().size();i++){
+			
+			if(tavolo.pushToTavoloControl(mano.getMano().get(i))){
+				Carta app=mano.getMano().get(i);
+				tavolo.pushToTavolo(app);
+				flag = 1;
+				break;
+			}
+		}
+		if(flag!=1){
+			mano.aggiungiCarta(mazzo.pesca());
+		}
 	}
 
 	@Override
 	public void subisciCambiaColore(Carta c) {
 		// TODO Auto-generated method stub
+		
 		
 	}
 
